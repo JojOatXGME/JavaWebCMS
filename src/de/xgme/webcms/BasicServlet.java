9523147844c08a3a1f0e8dfcc1fbe6ae4c9c4b83
JojOatXGME <johannes.spangenberg@hotmail.de>
2013-02-03 15:52:51 +0100

    Set up file hierarchy

---
+++ src/de/xgme/webcms/BasicServlet.java
@@ -0,0 +1,48 @@
+package de.xgme.webcms;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * The basic servlet which will only call the CMS to generate the response.
+ */
+@WebServlet(description = "The basic servlet which will call the CMS", urlPatterns = { "/*" })
+public class BasicServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		// TODO create instance of the CMS
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO handle request
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO handler request
+	}
+
+	/**
+	 * @see HttpServlet#doHead(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO let generate only head informations
+		super.doHead(request, response);
+	}
+
+}
